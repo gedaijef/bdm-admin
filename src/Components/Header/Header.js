37df36833d9255ce8ac7195b99aa85139eedfb12
GuLeiteAbreu007 <gustavo.abreu@germinare.org.br>
2024-12-10 08:05:46 -0300

    Ajeitando header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,23 @@
 import React from "react";
-import style from "./Header.module.css"
-import { useNavigate } from 'react-router-dom';
+import style from "./Header.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const navigate = useNavigate()
-  const redirecionamento = () =>{
-    navigate('/')
-  }
-
+  const navigate = useNavigate();
+  const redirecionamento = () => {
+    navigate("/");
+  };
 
   return (
     <div className={style.container}>
-      <div onClick={redirecionamento} className={style.content}>
-        <img
-          className={style.img}
-          src="https://www.bomdiamercado.com.br/wp-content/themes/bomdiamercado.com.br/assets/images/logo-bdm+picpay-black.svg"
-        ></img>
-        <h1>| ADMIN</h1>
+      <div className={style.content}>
+        <div onClick={redirecionamento} className={style.option}>
+          <img
+            className={style.img}
+            src="https://www.bomdiamercado.com.br/wp-content/themes/bomdiamercado.com.br/assets/images/logo-bdm+picpay-black.svg"
+          ></img>
+          <h1>| ADMIN</h1>
+        </div>
       </div>
     </div>
   );
